18024a4ef9cb460c59ab95f0e4a95c02006e7337
a158790 <robert_j_meester@progressive.com>
2024-09-03 20:33:22 +0000

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,20 +8,29 @@ from random import randint
 
 pytest_plugins = ["dbt.tests.fixtures.project"]
 
-dotenv_path = Path('test.env')
+dotenv_path = Path("test.env")
 load_dotenv(dotenv_path=dotenv_path)
+
 
 @pytest.fixture(scope="class")
 def dbt_profile_target():
     return {
-        'type': 'ibmdb2',
-        'threads': 1,
-        'host': os.getenv('DBT_DB2_HOST'),
-        'database': os.getenv('DBT_DB2_DATABASE'),
-        'schema': os.getenv('DBT_DB2_SCHEMA'),
-        'user': os.getenv('DBT_DB2_USERNAME'),
-        'password': os.getenv('DBT_DB2_PASSWORD'),
-        'port': int(os.getenv('DBT_DB2_PORT')),
-        'protocol': os.getenv('DBT_DB2_PROTOCOL'),
-        'extra_connect_opts': os.getenv('DBT_DB2_EXTRA_CONNECT_OPTS'),
+        "type": "ibmdb2",
+        "running_on": "z/os",
+        "authentication": "kerberos",
+        "dsn": "D_CMSKB1",
+        "database": "DSNDB06",
+        "schema": "SYSIBM",
     }
+    # return {
+    #     'type': 'ibmdb2',
+    #     'threads': 1,
+    #     'host': os.getenv('DBT_DB2_HOST'),
+    #     'database': os.getenv('DBT_DB2_DATABASE'),
+    #     'schema': os.getenv('DBT_DB2_SCHEMA'),
+    #     'user': os.getenv('DBT_DB2_USERNAME'),
+    #     'password': os.getenv('DBT_DB2_PASSWORD'),
+    #     'port': int(os.getenv('DBT_DB2_PORT')),
+    #     'protocol': os.getenv('DBT_DB2_PROTOCOL'),
+    #     'extra_connect_opts': os.getenv('DBT_DB2_EXTRA_CONNECT_OPTS'),
+    # }
